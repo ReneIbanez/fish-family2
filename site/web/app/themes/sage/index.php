7b215b530ce6470ce8cfa4dd1fa886032c7523a0
Rene Ibanez <reneibanez@resilients-MacBook-Pro.local>
2017-09-28 13:13:45 -0400

    most of the pages is complete just need to add final touches

--- site/web/app/themes/sage/index.php
+++ site/web/app/themes/sage/index.php
@@ -13,7 +13,7 @@ sort($posts);
 foreach($posts as $post) { 
     setup_postdata( $post );?>
 
-   <h2><?php  the_title(); ?></h2>
+   <h2 class="titleHeadings"><?php  the_title(); ?></h2>
     <p><?php the_content(); ?></p>
     <?php the_post_thumbnail() ?>
 <?php } ?>
